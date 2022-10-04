970dcba62f298cf55432a1517d1d617a5e1ea527
Timofey Luin <timofey.luin@gmail.com>
2022-10-04 16:25:37 +0300

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use discv5::{enr, enr::{CombinedKey, NodeId, Enr}, Discv5, Discv5Config, Discv5Event, Key};
+use discv5::{enr, enr::{CombinedKey, NodeId, Enr}, Discv5, Discv5Config, Discv5Event, Key, Discv5ConfigBuilder};
 use warp::Filter;
 use std::net::{Ipv4Addr, SocketAddr};
 use tokio::time;
@@ -7,12 +7,12 @@ use tracing::{info, log::warn, info_span, trace_span, Instrument};
 use std::sync::Arc;
 use std::thread::sleep;
 use std::time::Duration;
-use tracing_subscriber::fmt::format::FmtSpan;
-use cli_batteries::{version,};
+use cli_batteries::{version};
 use clap::Parser;
 use discv5::kbucket::Node;
 use enr::k256::elliptic_curve::bigint::Encoding;
 use enr::k256::U256;
+use itertools::Itertools;
 use strum::EnumString;
 
 mod types;
@@ -42,17 +42,6 @@ fn main() {
 }
 
 async fn app(options: Options) -> eyre::Result<()> {
-    // allows detailed logging with the RUST_LOG env variable
-    let filter_layer = tracing_subscriber::EnvFilter::try_from_default_env()
-        .or_else(|_| tracing_subscriber::EnvFilter::try_new("info"))
-        .unwrap();
-    let _ = tracing_subscriber::fmt()
-        .with_env_filter(filter_layer)
-        .with_span_events(FmtSpan::CLOSE)
-        .try_init();
-
-    // if there is an address specified use it
-
     let discv5_servers = {
         let address = options.ip_listen.parse::<Ipv4Addr>().unwrap();
         construct_and_start(&options, address, options.port_udp, options.node_count).await
@@ -66,7 +55,7 @@ async fn app(options: Options) -> eyre::Result<()> {
     let mut str = StreamMap::new();
     for (i, s) in discv5_servers.iter().enumerate() {
         let rec = ReceiverStream::new(s.event_stream().await.unwrap());
-        str.insert(format!("Stream {}", i), rec);
+        str.insert(format!("Stream {} ({})", i, s.local_enr().node_id().to_string()), rec);
     }
     let discv5_events_task = tokio::spawn(async move {
         loop {
@@ -93,7 +82,7 @@ async fn app(options: Options) -> eyre::Result<()> {
                         info!("Stream {}: Talk request received: {}", chan, req_msg);
                         let response = format!("Response: {}", req_msg);
                         req.respond(response.into_bytes()).unwrap();
-                    }
+                    },
                 }
             }
         }
@@ -103,20 +92,15 @@ async fn app(options: Options) -> eyre::Result<()> {
         let enrs = enrs_stats;
 
         loop {
+            play_simulation(&options, &discv5_servers, &enrs).await;
+
             let peer_count = discv5_servers
                 .iter()
                 .map(|s| s.connected_peers())
                 .collect::<Vec<_>>();
-            let met = discv5_servers
-                .iter()
-                .map(|s| s.metrics())
-                .collect::<Vec<_>>();
             println!("Peer Count: {:?}", peer_count);
             // println!("Metrics: {:?}", met);
-
-            play_simulation(&options, &discv5_servers, &enrs).await;
-
-            time::sleep(time::Duration::from_secs(5)).await;
+            sleep(Duration::from_secs(2))
         }
     });
 
@@ -134,25 +118,47 @@ async fn app(options: Options) -> eyre::Result<()> {
 
 #[derive(Debug, PartialEq, EnumString)]
 enum SimCase {
+    #[strum(serialize = "0")]
+    SequentiaDiscovery,
     #[strum(serialize = "1")]
     LinearRouting,
     #[strum(serialize = "2")]
     ClosestToValue
 }
 
-pub async fn play_simulation(opts: &Options, discv5_servers: &[Discv5], enrs: &Arc<Vec<Enr<CombinedKey>>>) {
+pub async fn play_simulation(opts: &Options, discv5_servers: &Vec<Discv5>, enrs: &Arc<Vec<Enr<CombinedKey>>>) {
     assert!(discv5_servers.len() > 2);
     info!("starting simulation case {:?}", opts.simulation_case);
 
     match opts.simulation_case {
-        SimCase::LinearRouting => {
-            let last_node = discv5_servers.last().unwrap();
-            let span = info_span!("routing", target_key=last_node.local_enr().node_id().to_string());
-            let found = discv5_servers[0].find_node(last_node.local_enr().node_id()).instrument(span).await.unwrap();
-            info!("found_nodes {}", found.len());
+        SimCase::SequentiaDiscovery => {
+            let num_servers = discv5_servers.len();
+            for (i, discv5) in discv5_servers.iter().enumerate() {
+                let nextnode = enrs[(i+1)%num_servers].clone().udp4().unwrap();
+                if i != num_servers - 1 {
+                    let predicate = Box::new(move |enr: &Enr<CombinedKey>| enr.udp4().unwrap() == nextnode.clone());
+
+                    let found_nodes = discv5.find_node_predicate(enrs[(i+1)%num_servers].node_id(), predicate, 1).await.unwrap();
+                    println!("Found nodes: {:?}", found_nodes);
+                }
+            }
+
             // send talkreq from first node to last node
             let resp = discv5_servers[0].talk_req(enrs[enrs.len()-1].clone(), b"123".to_vec(), format!("hello{}",0).into_bytes()).await.unwrap();
             info!("Got response: {}", String::from_utf8(resp).unwrap());
+            time::sleep(time::Duration::from_secs(5)).await;
+        },
+        SimCase::LinearRouting => {
+            let last_node = discv5_servers.last().unwrap();
+            let last_node_id = last_node.local_enr().node_id();
+            let span = info_span!("routing", target_key=last_node_id.to_string());
+            let last_node_upd4 = last_node.local_enr().udp4().unwrap().clone();
+            // let predicate = Box::new(move |enr: &Enr<CombinedKey>| enr.udp4().unwrap() == last_node_upd4);
+            let found = discv5_servers[0].find_node(last_node_id).instrument(span).await.unwrap();
+            info!("found_nodes {}", found.len());
+            // send talkreq from first node to last node
+            // let resp = discv5_servers[0].talk_req(enrs[enrs.len()-1].clone(), b"123".to_vec(), format!("hello{}",0).into_bytes()).await.unwrap();
+            // info!("Got response: {}", String::from_utf8(resp).unwrap());
         }
         SimCase::ClosestToValue => {
             let last_node_id = discv5_servers.last().unwrap().local_enr().node_id();
@@ -163,13 +169,6 @@ pub async fn play_simulation(opts: &Options, discv5_servers: &[Discv5], enrs: &A
                 NodeId::new(&raw.to_be_bytes())
             };
 
-            let d9 = Key::from(last_node_id).distance(&Key::from(distant_key));
-            let d2 = Key::from(discv5_servers[1].local_enr().node_id()).distance(&Key::from(distant_key));
-
-            println!("d2={:?}, d9={:?}, d2 > d9 = {}", d2, d9, d2 > d9);
-            // Key::from(last_node_id).distance(&Key::from(distant_key))
-
-
             let span = info_span!("routing", target_key=distant_key.to_string());
             let found = discv5_servers[0].find_node(distant_key).instrument(span).await.unwrap();
             info!("found_nodes {}", found.len());
@@ -179,13 +178,13 @@ pub async fn play_simulation(opts: &Options, discv5_servers: &[Discv5], enrs: &A
 
 async fn construct_and_start(
     opts: &Options,
-    listen_addr: Ipv4Addr,
+    listen_ip: Ipv4Addr,
     port_start: usize,
     node_count: usize,
 ) -> Vec<Discv5> {
     let mut discv5_servers = Vec::with_capacity(node_count);
     for i in 0..node_count {
-        let listen_addr = format!("{}:{}", listen_addr, port_start + i)
+        let listen_addr = format!("{}:{}", listen_ip, port_start + i)
             .parse::<SocketAddr>()
             .unwrap();
         println!("{}", listen_addr);
@@ -201,7 +200,7 @@ async fn construct_and_start(
             // if std::env::args().nth(2).is_some() {
             //     builder.udp4(port);
             // }
-            builder.ip4(format!("127.0.0.1").parse().unwrap());
+            builder.ip4(listen_ip);
             builder.udp4(port_start as u16 + i as u16);
             builder.build(&enr_key).unwrap()
         };
@@ -217,13 +216,18 @@ async fn construct_and_start(
             println!("ENR is not printed as no IP:PORT was specified");
         }
         // default configuration
-        let config = Discv5Config::default();
+        let mut config_builder = Discv5ConfigBuilder::default();
+        config_builder.request_retries(10);
+        config_builder.filter_max_nodes_per_ip(None);
+        let config = config_builder.build();
 
         // construct the discv5 server
         let discv5 = Discv5::new(enr, enr_key, config).unwrap();
         discv5_servers.push(discv5);
     }
-    set_topology(&opts, &discv5_servers);
+
+    discv5_servers = set_topology(&opts, discv5_servers);
+
     for s in discv5_servers.iter_mut() {
         let ip4 = s.local_enr().ip4().unwrap();
         let udp4 = s.local_enr().udp4().unwrap();
@@ -234,10 +238,16 @@ async fn construct_and_start(
     discv5_servers
 }
 
-pub fn set_topology(opts: &Options, discv5_servers: &[Discv5]) {
-    let enrs = discv5_servers.iter().map(|s|s.local_enr()).collect::<Vec<_>>();
+pub fn set_topology(opts: &Options, mut discv5_servers: Vec<Discv5>) -> Vec<Discv5> {
+    let last_node_id = Key::from(discv5_servers.last().unwrap().local_enr().node_id());
+
     match opts.topology {
         Topology::Linear => {
+            // sort peers based on xor-distance to the latest node
+            discv5_servers = discv5_servers.into_iter()
+                .sorted_by_key(|s| Key::from(s.local_enr().node_id()).distance(&last_node_id))
+                .rev().collect::<Vec<_>>();
+
             for (i, s) in discv5_servers.iter().enumerate() {
                 if i != discv5_servers.len() - 1 {
                     s.add_enr(discv5_servers[i+1].local_enr().clone()).unwrap()
@@ -245,4 +255,16 @@ pub fn set_topology(opts: &Options, discv5_servers: &[Discv5]) {
             }
         },
     }
+
+    discv5_servers.iter().enumerate().for_each(|(i,s)| {
+        println!("{}.id={}, i-1={:?}, n={}, known={:?}, d={:?}",
+                 i,
+                 s.local_enr().node_id().to_string(),
+                 discv5_servers.get((i as i32 -1) as usize).map(|e| e.local_enr().node_id().to_string()),
+                 s.table_entries().len(),
+                 s.table_entries_enr().first().map(|e|e.node_id().to_string()),
+                 Key::from(s.local_enr().node_id()).distance(&last_node_id));
+    });
+
+    discv5_servers
 }
